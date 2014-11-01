626bbacaf4e5daca5fc1767680f85ccfb6b6ff0a
woltbit <woltbit@gmail.com>
2014-11-01 18:08:10 +0800

    update

---
+++ Templates/index.php
@@ -0,0 +1,207 @@
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Woltbit -  Just Woltbit!</title>
+      <link href="css/bootstrap.css" rel="stylesheet">
+      <link href="css/woltbit.css" rel="stylesheet">
+      <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+  
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/woltbit.js"></script>
+    </head>
+    <body>
+      <nav class="navbar navbar-inverse nomb" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </nav>
+      
+      <div class="container">
+          <div class="sitename col-lg-3 col-md-3 col-sm-3 col-xs-12">
+              WOLTBIT
+          </div>
+          
+          <div class="adv-header col-lg-9 col-md-9 col-sm-9 hidden-xs">
+              Iklan
+          </div>
+          
+          <!--posts section-->
+          <!--main post section-->
+          <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="main-content col-md-8">
+                  <div class="main-post">
+                      <div class="main-post-image">
+                          <img src="images/show_ios7_gui.png" />
+                      </div>
+                      <div class="main-post-excerpt">
+                      	  <div class="catergory-path clearfix">
+                              <div class="category"><a href="#">iOS</a></div>
+                              <div class="views">Views : 123</div>
+                          </div>
+                          <div class="main-post-title">
+                              <a href="#">10 list Of Free & Nice iOS 7 GUI Templates</a>
+                          </div>
+                          <div class="main-post-content">
+                              <div class="main-post-text">
+                                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 
+                              </div>
+                          <button class="btn-info">Read More</button>
+                          </div>
+                      </div>
+                  </div><!--end of main-post-->
+                  <!--others post section-->
+                  <div class="others-post col-md-12">
+                          <div class="other-inner-post odd col-md-6">
+                              <div class="main-post">
+                                  <div class="main-post-image">
+                                      <img src="images/show_ios7_gui.png" />
+                                  </div>
+                                  <div class="main-post-excerpt">
+                                  	<div class="catergory-path clearfix">
+                                      <div class="category"><a href="#">iOS</a></div>
+                                      <div class="views">Views : 123</div>
+                                    </div>
+                                      <div class="main-post-title">
+                                          <a href="#">10 list Of Free & Nice iOS 7 GUI Templates</a>
+                                      </div>
+                                      <div class="main-post-content">
+                                          <div class="main-post-text">
+                                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                                          </div>
+                                      <button class="btn-info">Read More</button>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      
+                          <div class="other-inner-post even col-md-6">
+                              <div class="main-post">
+                                  <div class="main-post-image">
+                                      <img src="images/Andreas-Hellqvist iOS 7 GUI.jpg" />
+                                  </div>
+                                  <div class="main-post-excerpt">
+                                      <div class="catergory-path clearfix">
+                                      	<div class="category"><a href="#">iOS</a></div>
+                                      	<div class="views">Views : 123</div>
+                                  	  </div>
+                                      <div class="main-post-title">
+                                          <a href="#">10 list Of Free & Nice iOS 7 GUI Templates</a>
+                                      </div>
+                                      <div class="main-post-content">
+                                          <div class="main-post-text">
+                                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                          </div>
+                                      <button class="btn-info">Read More</button>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          
+                          <span class="col-md-12"><hr/></span>
+                          
+                          <div class="other-inner-post odd col-md-6">
+                              <div class="main-post">
+                                  <div class="main-post-image">
+                                      <img src="images/iOS7-1-teehan+lax.jpg" />
+                                  </div>
+                                  <div class="main-post-excerpt">
+                                  	  <div class="catergory-path clearfix">
+                                      	<div class="category"><a href="#">iOS</a></div>
+                                      	<div class="views">Views : 123</div>
+                                  	  </div>
+                                      <div class="main-post-title">
+                                          <a href="#">10 list Of Free & Nice iOS 7 GUI Templates</a>
+                                      </div>
+                                      <div class="main-post-content">
+                                          <div class="main-post-text">
+                                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                                          </div>
+                                      <button class="btn-info">Read More</button>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      
+                          <div class="other-inner-post even col-md-6">
+                              <div class="main-post">
+                                  <div class="main-post-image">
+                                      <img src="images/show_ios7_gui.png" />
+                                  </div>
+                                  <div class="main-post-excerpt">
+                                      <div class="catergory-path clearfix">
+                                      	<div class="category"><a href="#">iOS</a></div>
+                                      	<div class="views">Views : 123</div>
+                                  	  </div>
+                                      <div class="main-post-title">
+                                          <a href="#">10 list Of Free & Nice iOS 7 GUI Templates</a>
+                                      </div>
+                                      <div class="main-post-content">
+                                          <div class="main-post-text">
+                                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                          </div>
+                                      <button class="btn-info">Read More</button>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                  </div>
+                  <!--end of others post section-->
+                  
+                  <!-- pagination section-->
+                  <div class="pagination">
+                      <div class="btn-group">
+                          <button type="button" class="btn btn-default">&lt;</button>
+                          <button type="button" class="btn btn-default">1</button>
+                          <button type="button" class="btn btn-default">2</button>
+                          <button type="button" class="btn btn-default">3</button>
+                          <button type="button" class="btn btn-default">4</button>
+                          <button type="button" class="btn btn-default">5</button>
+                          <button type="button" class="btn btn-default">&gt;</button>
+                      </div>    
+                  </div>
+                  <!-- end of pagination section-->
+              </div><!--end of main-content col-md-8-->
+              <div class="sidebar col-md-4">
+                  <div class="mini-adv col-md-5 col-xs-5">
+                  	I
+                  </div>
+                  <div class="mini-adv col-md-5 col-xs-5">
+                  	I
+                  </div>
+                  <div class="mini-adv col-md-5 col-xs-5">
+                  	I
+                  </div>
+                  <div class="mini-adv col-md-5 col-xs-5">
+                  	I
+                  </div>
+              </div>
+          </div>
+          <!--end of main post section-->
+          
+          <!--end of posts section-->
+      </div>
+      
+      
+    </body>
+  </html>
---
+++ Templates/js/woltbit.js
@@ -0,0 +1,3 @@
+window.onresize = function(event) {
+  window.location.reload()
+};
